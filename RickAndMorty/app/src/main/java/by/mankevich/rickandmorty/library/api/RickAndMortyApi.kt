cfdf594a17005f5111b244247b27c2ebb4f98b1c
qqvladik <94757352+qqvladik@users.noreply.github.com>
2022-06-29 18:32:47 +0300

    Add fetchMultiple methods to api

--- RickAndMorty/app/src/main/java/by/mankevich/rickandmorty/library/api/RickAndMortyApi.kt
+++ RickAndMorty/app/src/main/java/by/mankevich/rickandmorty/library/api/RickAndMortyApi.kt
@@ -4,19 +4,41 @@ import by.mankevich.rickandmorty.library.api.response.*
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface RickAndMortyApi {
 
     @GET("character")
-    suspend fun fetchCharacters(): CharactersListResponse
-
-    @GET("character/{id}")
-    fun fetchCharacterById(@Path("id") id: Int): CharacterResponse
+    suspend fun fetchCharacters(
+        @Query("page") page: Int = 1,
+        @Query("name") name: String = "",
+        @Query("status") status: String = "",
+        @Query("species") species: String = "",
+        @Query("type") type: String = "",
+        @Query("gender") gender: String = ""
+    ): CharactersListResponse
 
     @GET("location")
-    suspend fun fetchLocations(): LocationsListResponse
+    suspend fun fetchLocations(
+        @Query("page") page: Int = 1,
+        @Query("name") name: String = "",
+        @Query("type") type: String = "",
+        @Query("dimension") dimension: String = ""
+    ): LocationsListResponse
 
     @GET("episode")
-    suspend fun fetchEpisodes(): EpisodesListResponse
+    suspend fun fetchEpisodes(
+        @Query("page") page: Int = 1,
+        @Query("name") name: String = "",
+        @Query("episode") episode: String = ""
+    ): EpisodesListResponse
 
+    @GET("character/{ids}")
+    suspend fun fetchMultipleCharacters(@Path("ids") ids: List<Int>): List<CharacterResponse>
+
+    @GET("location/{ids}")
+    suspend fun fetchMultipleLocations(@Path("ids") ids: List<Int>): List<CharacterResponse>
+
+    @GET("episode/{ids}")
+    suspend fun fetchMultipleEpisodes(@Path("ids") ids: List<Int>): List<CharacterResponse>
 }
